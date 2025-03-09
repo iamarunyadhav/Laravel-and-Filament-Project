d13ac04505d9a2c60f28a0d8ba20cd6bd30a4cec
arunpragash <imarunpragash@gmail.com>
2025-03-09 18:48:38 +0530

    dashboard theme changes migration order laravel cloud

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('products', ProductController::class);
 
